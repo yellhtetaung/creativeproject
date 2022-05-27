6ea2b146a21cf072e2593169d78ed22583505fee
yellhtetaung <yairhtet248.yha@gmail.com>
2022-05-27 22:19:16 +0630

    27 May 2022

---
+++ js/app.js
@@ -0,0 +1,28 @@
+
+let scrollPosition = 0;
+let navBar = document.getElementsByTagName('nav')[0].classList;
+
+// for scrollY
+document.addEventListener('scroll', function (){
+    scrollPosition = window.scrollY;
+    navBar.add('bg-light');
+    navBar.replace('navbar-dark','navbar-light');
+    if (scrollPosition === 0){
+        if(window.innerWidth >= 991){
+            navBar.remove("bg-light");
+            navBar.replace('navbar-light','navbar-dark');
+        }
+    }
+})
+
+// for navbar md view
+window.addEventListener("resize", (function onResize () {
+    if (window.innerWidth <= 991){
+        navBar.add('bg-light');
+        navBar.replace('navbar-dark','navbar-light');
+    } else if(window.innerWidth >= 991){
+        navBar.remove("bg-light");
+        navBar.replace('navbar-light','navbar-dark');
+    }
+    return onResize;
+})());
